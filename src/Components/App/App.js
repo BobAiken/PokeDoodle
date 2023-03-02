f126d2a05a3fe1fa4fdfe9d2c43c38fe55898245
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-02 14:05:27 -0600

    Fix pokemon not having the proper ID

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,26 @@ import Header from '../Header/Header';
 function App() {
 
   const [pokemonList, setPokemonList] = useState([])
+  const [pokemonDetails, setPokemonDetails] = useState([])
   
   useEffect(()=>{
     fetchPokemon('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
-    .then(data => {setPokemonList(data.results)})},[])
+    .then(data => {setPokemonList(data.results)})
+    },[])
+
+  // useEffect(()=>{
+  //   const fetchArray = pokemonList.map(pokemon => {
+  //     console.log(pokemon.url)
+  //     fetch(pokemon.url)
+  //   })
+  //   console.log(fetchArray)
+  //   Promise.all(fetchArray)
+  //   .then((response) => {
+  //     Promise.all(response.map((item) => {
+  //       return item.json()}))
+  //   })
+  //   .then(data => console.log(data))
+  // },[pokemonList])
 
   return (
    <main>
--- src/Components/Pokemon/PokemonList.js
+++ src/Components/Pokemon/PokemonList.js
@@ -3,7 +3,7 @@ import PokemonCard from "../PokemonCard/PokemonCard"
 import fetchPokemon from "../../apiCalls"
 import "./PokemonList.css"
 
-function PokemonList({pokemonList,filterPokemon}){
+function PokemonList({pokemonList}){
 
   const [query, setQuery] = useState('')
 
@@ -21,16 +21,24 @@ function PokemonList({pokemonList,filterPokemon}){
     }
   })
   .map((pokemon,index) => {
-      return(
-        <PokemonCard
-          id={index+1}
-          key={index+1}
-          name={pokemon.name}
-          img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index+1}.png`}
-        />
+    let id
+    if(pokemon.url.length === 36) {
+      id = pokemon.url.slice(-2,-1)
+    } else if (pokemon.url.length === 37) {
+      id = pokemon.url.slice(-3,-1)
+    } else {
+      id = pokemon.url.slice(-4,-1)
+    }
+    return(
+      <PokemonCard
+      id={id}
+      key={id}
+      name={pokemon.name}
+      img={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+      />
       )
     }
-  )
+    )
 
   return (
     <div className="pokemon-container">
