c36c2c48c528c213b0f969f0df9b2b62e358f6df
Robert Aiken <robertsiraaiken@gmail.com>
2023-03-02 07:46:48 -0600

    Add header

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import React, {useEffect, useState} from "react"
 import fetchPokemon from '../../apiCalls';
 import PokemonList from '../Pokemon/PokemonList';
 import { Route, Routes } from "react-router-dom"
+import Header from '../Header/Header';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
 
   return (
    <main>
+    <Header/>
     <Routes>
       <Route exact path="/" element={
         <PokemonList pokemonList={pokemonList}/>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <Link to="/">
+        PokeDoodle  
+      </Link>
+    </div>
+  )
+}
